a636e210ff13dd6df165461a358178332a2affdd
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2022-05-06 21:48:53 +0530

    Refactor

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
-require('./services/passport');
-
 const app = express();
 
 require('./routes/authRoutes')(app);
+require('./services/passport');
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT);
